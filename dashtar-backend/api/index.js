38de1b79bbec9cee20e87069ed466239bc527093
Aditya Sharma <adityaanilsharma00@gmail.com>
2023-05-19 15:25:48 +0530

    first

---
+++ dashtar-backend/api/index.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const path = require('path');
+const { connectDB } = require("../config/db");
+const productRoutes = require("../routes/productRoutes");
+const customerRoutes = require("../routes/customerRoutes");
+const adminRoutes = require("../routes/adminRoutes");
+const orderRoutes = require("../routes/orderRoutes");
+const customerOrderRoutes = require("../routes/customerOrderRoutes");
+const categoryRoutes = require("../routes/categoryRoutes");
+const couponRoutes = require("../routes/couponRoutes");
+const attributeRoutes = require("../routes/attributeRoutes");
+const settingRoutes = require("../routes/settingRoutes");
+const currencyRoutes = require("../routes/currencyRoutes");
+const languageRoutes = require("../routes/languageRoutes");
+const { isAuth, isAdmin } = require("../config/auth");
+
+connectDB();
+const app = express();
+
+// We are using this for the express-rate-limit middleware
+// See: https://github.com/nfriedly/express-rate-limit
+// app.enable('trust proxy');
+app.set("trust proxy", 1);
+
+app.use(express.json({ limit: "4mb" }));
+app.use(helmet());
+app.use(cors());
+
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname ,"../dashtar-admin/build"); 
+
+app.use(express.static (buildPath));
+
+app.get("/*", function (req, res){
+   res.sendFile( path.join(_dirname, "../dashtar-admin/build/index.html"),
+    function (err) { 
+      if (err) { res.status (500).send(err); } }
+       );
+       });
+
+
+//root route
+app.get("/", (req, res) => {
+  res.send("App works properly!");
+});
+
+//this for route will need for store front, also for admin dashboard
+app.use("/api/products/", productRoutes);
+app.use("/api/category/", categoryRoutes);
+app.use("/api/coupon/", couponRoutes);
+app.use("/api/customer/", customerRoutes);
+app.use("/api/order/", isAuth, customerOrderRoutes);
+app.use("/api/attributes/", attributeRoutes);
+app.use("/api/setting/", settingRoutes);
+app.use("/api/currency/", isAuth, currencyRoutes);
+app.use("/api/language/", languageRoutes);
+
+//if you not use admin dashboard then these two route will not needed.
+app.use("/api/admin/", adminRoutes);
+app.use("/api/orders/", orderRoutes);
+
+// Use express's default error handling middleware
+app.use((err, req, res, next) => {
+  if (res.headersSent) return next(err);
+  res.status(400).json({ message: err.message });
+});
+
+const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
